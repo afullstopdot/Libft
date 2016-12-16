410debea2578a55b423e4ae396e437ea410703ef
afullstopdot <andreantoniomarques19@gmail.com>
2016-12-16 13:04:32 +0200

    Fixed a few more of the functions, strtrim and sub wernt working cause of bzero lol

--- src/strncpy.c
+++ src/strncpy.c
@@ -8,22 +8,15 @@
 
 char		*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	size_t	i;
-	size_t	j;
 	size_t	k;
 
 	k = 0;
-	i = ft_strlen((char *)dest);
-	j = ft_strlen((char *)src);
-	if (i >= j)
+	while (src[k] && k < n)
 	{
-		while (src[k] && k < n)
-		{
-			dest[k] = ((char *)src)[k];
-			k++;
-		}
-		while (k < n)
-			dest[k++] = '\0';
+		dest[k] = ((char *)src)[k];
+		k++;
 	}
+	while (k < n)
+		dest[k++] = '\0';
 	return (dest);
 }
